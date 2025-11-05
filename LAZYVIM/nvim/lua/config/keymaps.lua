a75ec02a5ccc6565d0e04c349002dbe75774f81d
Ronil Maharjan <ronilmaharjan12@gmail.com>
2025-11-05 15:48:35 +0545

    update

--- LAZYVIM/nvim/lua/config/keymaps.lua
+++ LAZYVIM/nvim/lua/config/keymaps.lua
@@ -9,3 +9,43 @@ local unmap = vim.keymap.del
 map("i", "jj", "<Esc>", { noremap = false })
 map("n", "<leader>z", "ggVG", { desc = "Select all" })
 map("n", "<leader>k", ":b#<CR>", { desc = "Switch to alternate file" })
+
+---------------------
+-- Logging keymaps --
+---------------------
+map("n", "<leader>cp", function()
+  local ft = vim.bo.filetype
+  local var = vim.fn.getreg('"')
+  local log = ""
+
+  if ft == "javascript" or ft == "typescript" or ft == "javascriptreact" or ft == "typescriptreact" then
+    log = string.format('console.log("Logging %s", %s);', var, var)
+  elseif ft == "python" then
+    log = string.format('print("Logging %s", %s)', var, var)
+  elseif ft == "lua" then
+    log = string.format('print("Logging %s", %s)', var, var)
+  elseif ft == "go" then
+    log = string.format('fmt.Println("Logging %s", %s)', var, var)
+  else
+    log = string.format("-- No log format for %s", ft)
+  end
+
+  vim.api.nvim_put({ log }, "l", true, true)
+end, { desc = "Insert language-specific log statement" })
+
+-- Map n (next match) to center the cursor as well
+map("n", "n", "nzz", { desc = "Next search result and center cursor" })
+-- Map N (previous match) to center the cursor as well
+map("n", "N", "Nzz", { desc = "Previous search result and center cursor" })
+
+-- quickfix navigation
+map("n", "<C-j>", "<cmd>cnext<CR>zz", { desc = "Quickfix Next" })
+map("n", "<C-k>", "<cmd>cprev<CR>zz", { desc = "Quickfix Prev" })
+
+-- LocList navigation
+map("n", "<M-C-j>", "<cmd>lnext<CR>zz", { desc = "Quickfix Next" })
+map("n", "<M-C-k>", "<cmd>lprev<CR>zz", { desc = "Quickfix Prev" })
+
+-- keep cursor at middle when jumping half page
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
--- LAZYVIM/nvim/lua/plugins/dashboard.lua
+++ LAZYVIM/nvim/lua/plugins/dashboard.lua
@@ -14,6 +14,33 @@ return {
         ]],
       },
     },
+    picker = {
+      actions = {
+        explorer_open_windows = function(_, item)
+          if not item then
+            return
+          end
+
+          local path = item.file:gsub("/", "\\")
+          if item.dir then
+            vim.fn.system(string.format('cmd /C start "" "%s"', path))
+          else
+            vim.fn.system(string.format('cmd /C explorer.exe /select,"%s"', path))
+          end
+        end,
+      },
+      sources = {
+        explorer = {
+          win = {
+            list = {
+              keys = {
+                ["O"] = "explorer_open_windows",
+              },
+            },
+          },
+        },
+      },
+    },
   },
   keys = {
     {
@@ -25,4 +52,3 @@ return {
     },
   },
 }
--- opts.picker.sources.explorer.layout.layout.width = 10
