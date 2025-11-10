ead5291d8f06ca516825af56bd503f14ef647443
Ronil Maharjan <ronilmaharjan12@gmail.com>
2025-11-10 21:50:02 +0545

    show blink completion

---
+++ LAZYVIM/nvim/lua/plugins/blink.lua
@@ -0,0 +1,13 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      ["<C-b>"] = { "show" },
+    },
+    completion = {
+      trigger = {
+        show_on_trigger_character = true,
+      },
+    },
+  },
+}
